9c9556557e3dfa9e8769d0ba289959153a80c975
Kamar-Bazzi <kamarbazzi08@example.com>
2026-07-22 00:55:27 +0300

    Add LoginPage and setup routing

---
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
+      <h1>Medical Tracking System</h1>
+      <h2>Login Page</h2>
+      <p>Please enter your credentials to access your account.</p>
+    </div>
+  );
+}
